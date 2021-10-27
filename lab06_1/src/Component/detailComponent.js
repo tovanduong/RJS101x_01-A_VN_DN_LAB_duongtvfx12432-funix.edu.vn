8e60cf9fba05a365e8e6c261fb39a4fc4c3a6a0c
duong <tovanduonghp1996@gmail.com>
2021-10-27 09:58:13 -0700

    add lab06

---
+++ lab06_1/src/Component/detailComponent.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+// import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+function RenderDish({dish}) {
+    return (
+        <div className="col-12 col-md-5 m-1" key={dish.id}>
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+//e viet ham chi tiet mon an khi click no ra
+// code ko co click nhng van chay dc
+//đợi tý nó cài xong là đc. em cứ next giúp anh nhé
+
+
+const DishDetail = (props) => {
+    if (props.dish != null)        
+    return (
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                <RenderDish dish={props.dish} />
+            </div>
+        </div>
+    );
+else
+    return(
+        <div></div>
+    );
+
+}
+export default DishDetail;
---
+++ lab06_1/src/Component/mainComponent.js
@@ -0,0 +1,62 @@
+import { DISHES } from '../shared/dishes'
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import { COMMENTS } from '../shared/comments';
+import DishDetail from './detailComponent';
+import { Component } from 'react';
+import Home from './homeComponent';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './contactComponent';
+import About from './aboutComponent';
+import Header from './headerComponent';
+import Footer from './footerComponent';
+import Menu from './menuComponent';
+import history from '../helps/history';
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        }
+    }
+    render() {
+        const HomePage = () => {
+            return (
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+        }
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} />
+            )
+        }
+        return (
+            <div>
+                <Router history={history}>
+                    <div>
+                        <Header />
+                        <Switch>
+                            <Route path='/home' component={HomePage} />
+                            <Route exact path='/menu' component={() => <Menu dish={this.state.dishes} />} />
+                            <Route path='/menu/:dishId' component={DishWithId} />
+                            <Route exact path='/contactus' component={Contact} />
+                            <Route exact path='/Aboutus' component={() => <About leaders={this.state.leaders} />}/>
+                            <Redirect to="/home" />
+                        </Switch>
+                        <Footer />
+                    </div>
+                </Router>
+            </div>
+        );
+    }
+
+}
+export default Main;
+
